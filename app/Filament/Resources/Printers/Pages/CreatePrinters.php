a4e6b0cea1428d13762d2a3871aafa4edb1420c7
glenoka <glanokaparisudha@gmail.com>
2025-09-18 16:50:46 +0800

    feat : registerprinter cpus server

--- app/Filament/Resources/Printers/Pages/CreatePrinters.php
+++ app/Filament/Resources/Printers/Pages/CreatePrinters.php
@@ -2,10 +2,29 @@
 
 namespace App\Filament\Resources\Printers\Pages;
 
-use App\Filament\Resources\Printers\PrintersResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\Printers\PrintersResource;
 
 class CreatePrinters extends CreateRecord
 {
     protected static string $resource = PrintersResource::class;
+    protected function afterCreate(): void
+    {
+        $data = $this->form->getState();
+        $printername = preg_replace('/[^A-Za-z0-9]/', '_', $data['name']);
+        $cmd = sprintf(
+            'lpadmin -p %s -E -v socket://%s:%s -m raw',
+            escapeshellarg($printername),
+            escapeshellarg($data['ip_address']),
+            escapeshellarg($data['port'])
+        );
+        exec($cmd);
+    
+        // Aktifkan printer
+        exec("cupsenable " . escapeshellarg($printername));
+        exec("cupsaccept " . escapeshellarg($printername));
+    
+      
+    }
 }
